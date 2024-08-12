334f0f41eb5df9c0ec52a0758864e7e3bc95432e
ahmedbasemdev <ahmedbasem.dev@gmail.com>
2024-08-12 12:25:05 +0300

    feat: text data cleaning

--- utils/clean_data.py
+++ utils/clean_data.py
@@ -1,18 +1,27 @@
 import re
 
-def remove_link(text):
-    text = text
+def remove_link_hashtag_mentions(text):
+    # Remove URLs (links)
+    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
+    
+    # Remove Twitter mentions (e.g., @username)
+    text = re.sub(r'@\w+', '', text)
+    
+    # Remove hashtags (e.g., #hashtag)
+    text = re.sub(r'#\w+', '', text)
+    
+    # Remove any remaining special characters and extra whitespace
+    text = re.sub(r'\s+', ' ', text).strip()
 
     return text
 
-def remove_hashtag_mentions(text):
-    text = text
 
-    return
 
 
 def text_process_pipeline(text):
-    text = remove_link(text)
-    text = remove_hashtag_mentions(text)
+    text = remove_link_hashtag_mentions(text)
+    text = text.strip()
+    if len(text.split() < 10):
+        return None
 
     return text
